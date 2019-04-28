5c7224ebec266101457b749da137a57945dbf4cf
David <danaya@gmail.com>
2019-04-28 18:26:31 +0200

    fix(dependencies): fix yaml dependencies

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,7 +1,9 @@
-import 'package:example/basic.dart';
-import 'package:example/roulette.dart';
+import 'dart:async';
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_spinning_wheel/flutter_spinning_wheel.dart';
 
 void main() {
   SystemChrome.setEnabledSystemUIOverlays([]);
@@ -71,5 +73,135 @@ class MyHomePage extends StatelessWidget {
         style: TextStyle(color: Colors.white, fontSize: 18.0),
       ),
     );
+  }
+}
+
+class Basic extends StatelessWidget {
+  final StreamController _dividerController = StreamController<int>();
+
+  dispose() {
+    _dividerController.close();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(backgroundColor: Color(0xffB0F9D2), elevation: 0.0),
+      backgroundColor: Color(0xffB0F9D2),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SpinningWheel(
+              Image.asset('assets/images/wheel-6-300.png'),
+              width: 310,
+              height: 310,
+              initialSpinAngle: _generateRandomAngle(),
+              spinResistance: 0.2,
+              dividers: 6,
+              onUpdate: _dividerController.add,
+              onEnd: _dividerController.add,
+            ),
+            StreamBuilder(
+              stream: _dividerController.stream,
+              builder: (context, snapshot) =>
+                  snapshot.hasData ? BasicScore(snapshot.data) : Container(),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  double _generateRandomAngle() => Random().nextDouble() * pi * 2;
+}
+
+class BasicScore extends StatelessWidget {
+  final int selected;
+
+  final Map<int, String> labels = {
+    1: 'Purple',
+    2: 'Magenta',
+    3: 'Red',
+    4: 'Dark Orange',
+    5: 'Light Orange',
+    6: 'Yellow',
+  };
+
+  BasicScore(this.selected);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text('${labels[selected]}',
+        style: TextStyle(fontStyle: FontStyle.italic));
+  }
+}
+
+class Roulette extends StatelessWidget {
+  final StreamController _dividerController = StreamController<int>();
+
+  dispose() {
+    _dividerController.close();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(backgroundColor: Color(0xffDDC3FF), elevation: 0.0),
+      backgroundColor: Color(0xffDDC3FF),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SpinningWheel(
+              Image.asset('assets/images/roulette-8-300.png'),
+              width: 310,
+              height: 310,
+              initialSpinAngle: _generateRandomAngle(),
+              spinResistance: 0.6,
+              canInteractWhileSpinning: false,
+              dividers: 8,
+              onUpdate: _dividerController.add,
+              onEnd: _dividerController.add,
+              secondaryImage:
+                  Image.asset('assets/images/roulette-center-300.png'),
+              secondaryImageHeight: 110,
+              secondaryImageWidth: 110,
+            ),
+            SizedBox(height: 30),
+            StreamBuilder(
+              stream: _dividerController.stream,
+              builder: (context, snapshot) =>
+                  snapshot.hasData ? RouletteScore(snapshot.data) : Container(),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  double _generateRandomAngle() => Random().nextDouble() * pi * 2;
+}
+
+class RouletteScore extends StatelessWidget {
+  final int selected;
+
+  final Map<int, String> labels = {
+    1: '1000\$',
+    2: '400\$',
+    3: '800\$',
+    4: '7000\$',
+    5: '5000\$',
+    6: '300\$',
+    7: '2000\$',
+    8: '100\$',
+  };
+
+  RouletteScore(this.selected);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text('${labels[selected]}',
+        style: TextStyle(fontStyle: FontStyle.italic, fontSize: 24.0));
   }
 }
